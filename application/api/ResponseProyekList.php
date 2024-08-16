396e6cb9beac5d2a9eb6ef6734856a661e908a8c
Thito Yalasatria Sunarya <septogeddon@gmail.com>
2024-08-16 16:18:06 +0700

    Added api

---
+++ application/api/ResponseProyekList.php
@@ -0,0 +1,10 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+class ResponseProyekList extends BasicResponse {
+	public $data;
+
+	public function __construct($status, $message, $data) {
+		parent::__construct($status, $message);
+		$this->data = $data;
+	}
+}
